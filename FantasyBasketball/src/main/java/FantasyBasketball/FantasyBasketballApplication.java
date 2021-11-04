697fe5f16d8a72a68e6f2526f776d3dd7f9d9aee
Daktor99 <ed2918@columbia.edu>
2021-11-04 01:20:47 -0400

    Added Spring Boot Skeleton

---
+++ FantasyBasketball/src/main/java/FantasyBasketball/FantasyBasketballApplication.java
@@ -0,0 +1,13 @@
+package FantasyBasketball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyBasketballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyBasketballApplication.class, args);
+	}
+
+}
---
+++ FantasyBasketball/src/test/java/FantasyBasketball/FantasyBasketballApplicationTests.java
@@ -0,0 +1,13 @@
+package FantasyBasketball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyBasketballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
