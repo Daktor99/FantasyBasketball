4bcb451340bb99ff28d022628e6ce2d6874642c5
Daktor99 <ed2918@columbia.edu>
2021-11-09 21:18:48 -0500

    Added find by template query for userRepository

--- FantasyBasketball/src/main/java/FantasyBasketball/repositories/userRepository.java
+++ FantasyBasketball/src/main/java/FantasyBasketball/repositories/userRepository.java
@@ -1,8 +1,12 @@
 package FantasyBasketball.repositories;
 
 import FantasyBasketball.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /*
     CrudRepository already has the following functions:
@@ -26,4 +30,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface userRepository extends CrudRepository<User, Integer> {
 
+    @Query(value = "select * from user where ((:user_id is NULL or user_id = :user_id) and\n" +
+            "                          (:email is NULL or email = :email) and\n" +
+            "                          (:username is NULL or username = :username) and\n" +
+            "                          (:first_name is NULL or first_name = :first_name) and\n" +
+            "                          (:last_name is NULL or last_name = :last_name))", nativeQuery = true)
+    List<User> findByTemplate(@Param("user_id") Integer user_id,
+                              @Param("email") String email,
+                              @Param("username") String username,
+                              @Param("first_name") String first_name,
+                              @Param("last_name") String last_name);
 }
