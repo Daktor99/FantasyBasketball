374869962edece70ec547731d40fb167cdcd0d75
Daktor99 <ed2918@columbia.edu>
2021-11-11 18:38:41 -0500

    Refined fantasyGameRepository query

--- FantasyBasketball/src/main/java/FantasyBasketball/repositories/fantasyGameRepository.java
+++ FantasyBasketball/src/main/java/FantasyBasketball/repositories/fantasyGameRepository.java
@@ -31,51 +31,19 @@ import java.util.List;
 @Repository
 public interface fantasyGameRepository extends CrudRepository<FantasyGame, Integer> {
 
-    @Query(value = "select * from user where ((:schedule_id is NULL or schedule_id = :schedule_id) and\n" +
-                                            "(:league_id is NULL or league_id = :league_id) and\n" +
-                                            "(:home_team_id is NULL or home_team_id = :home_team_id) and\n" +
-                                            "(:away_team_id is NULL or away_team_id = :away_team_id) and\n" +
-                                            "(:game_start_date is NULL or game_start_date = :game_start_date))" +
-                                            "(:game_end_date is NULL or game_end_date = :game_end_date) and\n" +
-                                            "(:winner_id is NULL or winner_id = :winner_id) and\n" +
-                                            "(:home_points is NULL or home_points = :home_points) and\n" +
-                                            "(:away_points is NULL or away_points = :away_points) and\n" +
-                                            "(:home_start_pg_id is NULL or home_start_pg_id = :home_start_pg_id) and\n" +
-                                            "(:home_start_sg_id is NULL or home_start_sg_id = :home_start_sg_id) and\n" +
-                                            "(:home_start_sf_id is NULL or home_start_sf_id = :home_start_sf_id) and\n" +
-                                            "(:home_start_pf_id is NULL or home_start_pf_id = :home_start_pf_id) and\n" +
-                                            "(:home_start_c_id is NULL or home_start_c_id = :home_start_c_id) and\n" +
-                                            "(:home_bench_1_id is NULL or home_bench_1_id = :home_bench_1_id) and\n" +
-                                            "(:home_bench_2_id is NULL or home_bench_2_id = :home_bench_2_id) and\n" +
-                                            "(:away_start_pg_id is NULL or away_start_pg_id = :away_start_pg_id) and\n" +
-                                            "(:away_start_sg_id is NULL or away_start_sg_id = :away_start_sg_id) and\n" +
-                                            "(:away_start_sf_id is NULL or away_start_sf_id = :away_start_sf_id) and\n" +
-                                            "(:away_start_pf_id is NULL or away_start_pf_id = :away_start_pf_id) and\n" +
-                                            "(:away_start_c_id is NULL or away_start_c_id = :away_start_c_id) and\n" +
-                                            "(:away_bench_1_id is NULL or away_bench_1_id = :away_bench_1_id) and\n" +
-                                            "(:away_bench_2_id is NULL or away_bench_2_id = :away_bench_2_id) and\n",
-                                            nativeQuery = true)
-    List<FantasyGame> findByTemplate(@Param("schedule_id")      Integer schedule_id,
-                              @Param("league_id")        Integer league_id,
-                              @Param("home_team_id")     Integer home_team_id,
-                              @Param("away_team_id")     Integer away_team_id,
-                              @Param("game_start_date")  Date game_start_date,
-                              @Param("game_end_date")    Date game_end_date,
-                              @Param("winner_id")        Integer winner_id,
-                              @Param("home_points")      Integer home_points,
-                              @Param("away_points")      Integer away_points,
-                              @Param("home_start_pg_id") Integer home_start_pg_id,
-                              @Param("home_start_sg_id") Integer home_start_sg_id,
-                              @Param("home_start_sf_id") Integer home_start_sf_id,
-                              @Param("home_start_pf_id") Integer home_start_pf_id,
-                              @Param("home_start_c_id")  Integer home_start_c_id,
-                              @Param("home_bench_1_id")  Integer home_bench_1_id,
-                              @Param("home_bench_2_id")  Integer home_bench_2_id,
-                              @Param("away_start_pg_id") Integer away_start_pg_id,
-                              @Param("away_start_sg_id") Integer away_start_sg_id,
-                              @Param("away_start_sf_id") Integer away_start_sf_id,
-                              @Param("away_start_pf_id") Integer away_start_pf_id,
-                              @Param("away_start_c_id")  Integer away_start_c_id,
-                              @Param("away_bench_1_id")  Integer away_bench_1_id,
-                              @Param("away_bench_2_id")  Integer away_bench_2_id);
+    @Query(value = "select * from fantasy_game where ((:schedule_id is NULL or schedule_id = :schedule_id) and\n" +
+                                                "(:league_id is NULL or league_id = :league_id) and\n" +
+                                                "(:home_team_id is NULL or home_team_id = :home_team_id) and\n" +
+                                                "(:away_team_id is NULL or away_team_id = :away_team_id) and\n" +
+                                                "(:game_start_date is NULL or game_start_date = :game_start_date))" +
+                                                "(:game_end_date is NULL or game_end_date = :game_end_date) and\n" +
+                                                "(:winner_id is NULL or winner_id = :winner_id))",
+                                                nativeQuery = true)
+    List<FantasyGame> findByTemplate(@Param("schedule_id")          Integer schedule_id,
+                                  @Param("league_id")               Integer league_id,
+                                  @Param("home_team_id")            Integer home_team_id,
+                                  @Param("away_team_id")            Integer away_team_id,
+                                  @Param("game_start_date")         Date game_start_date,
+                                  @Param("game_end_date")           Date game_end_date,
+                                  @Param("winner_id")               Integer winner_id);
 }
