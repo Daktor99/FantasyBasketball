194325e4f4940d97f3db8c77a4d31fc36bd8502e
pap2154 <pap2154@columbia.edu>
2021-11-11 18:49:20 -0500

    Added findByTemplate query for FantasyTeam

--- FantasyBasketball/src/main/java/FantasyBasketball/repositories/fantasyTeamRepository.java
+++ FantasyBasketball/src/main/java/FantasyBasketball/repositories/fantasyTeamRepository.java
@@ -1,8 +1,12 @@
 package FantasyBasketball.repositories;
 
 import FantasyBasketball.models.FantasyTeam;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /*
     CrudRepository already has the following functions:
@@ -25,5 +29,15 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface fantasyTeamRepository extends CrudRepository<FantasyTeam, Integer> {
+
+    @Query(value = "select * from fantasy_team where ((:team_id is NULL or team_id = :team_id) and\n" +
+            "                          (:team_name is NULL or team_name LIKE :team_name) and\n" +
+            "                          (:owner_id is NULL or owner_id = :owner_id) and\n" +
+            "                          (:league_id is NULL or league_id = :league_id))",
+            nativeQuery = true)
+    List<FantasyTeam> findByTemplate(@Param("team_id") Integer team_id,
+                              @Param("team_name") String team_name,
+                              @Param("owner_id") Integer owner_id,
+                              @Param("league_id") Integer league_id);
 
 }
