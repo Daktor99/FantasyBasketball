df4780c149709993062de7a464eb43894d00c205
pap2154 <pap2154@columbia.edu>
2021-11-10 22:28:25 -0500

    Updated UserSevice to throw correct exceptions and handle inputs

--- FantasyBasketball/src/main/java/FantasyBasketball/services/userService.java
+++ FantasyBasketball/src/main/java/FantasyBasketball/services/userService.java
@@ -1,7 +1,10 @@
 package FantasyBasketball.services;
 
+import FantasyBasketball.exceptions.userException;
+import FantasyBasketball.exceptions.userNotFoundException;
 import FantasyBasketball.models.User;
 import FantasyBasketball.repositories.userRepository;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +19,15 @@ public class userService {
     private userRepository userRepo;
 
     // find by ID
-    public List<User> getByID(Integer userID) {
+    public List<User> getByID(Integer userID) throws userNotFoundException {
         Optional<User> result = userRepo.findById(userID);
         if (result.isPresent()) {
             User userResult = result.get();
             return List.of(userResult);
         }
-        return Collections.emptyList();
+        else {
+            throw new userNotFoundException("User not found by ID in DB, cannot update");
+        }
     }
 
     // get operation
@@ -42,21 +47,70 @@ public class userService {
     }
 
     // put operation
-    public List<User> updateUser(User user) throws IllegalArgumentException {
-        if (getByID(user.getUserID()).size() >= 1) {
+    public List<User> updateUser(User user) throws userNotFoundException {
+        if(userRepo.existsById(user.getUserID())) {
             User result = userRepo.save(user);
             return List.of(result);
         } else {
-            throw new IllegalArgumentException("Resource not found by ID in DB, cannot update");
+            throw new userNotFoundException("User not found by ID in DB, cannot update");
         }
     }
 
     // delete operation
-    public void deleteUserById(Integer user_id) {
-        if (getByID(user_id).size() == 1) {
+    public void deleteUserById(Integer user_id) throws userNotFoundException {
+        if(userRepo.existsById(user_id)) {
             userRepo.deleteById(user_id);
         } else {
-            throw new IllegalArgumentException("Resource not found in DB, cannot delete");
+            throw new userNotFoundException("User not found in DB, cannot delete");
         }
+    }
+
+    //uses email validator tool to check if email is valid
+    public boolean isValidEmail(String email) {
+        return EmailValidator.getInstance().isValid(email);
+    }
+
+    // checking:    - length of string is <128 characters
+    //              - that string is not blank ex "   "
+    //              - string is initialize (not == null)
+    // returns true if follows rules above, false otherwise
+    public boolean checkIfInvalid(String string) {
+        return string.length() > 128 || string.isBlank();
+    }
+
+    // check and sanitize inputs
+    private void checkInputs(User user) throws userException {
+
+        try {
+            if (!isValidEmail(user.getEmail())) {
+                throw new userException("Email is invalid");
+            } else if (checkIfInvalid(user.getEmail())) {
+                throw new userException("Email must be between 1-128 characters.");
+            } else if (checkIfInvalid(user.getUsername())) {
+                throw new userException("Username must be between 1-128 characters.");
+            } else if (checkIfInvalid(user.getFirstName())) {
+                throw new userException("First name must be between 1-128 characters.");
+            } else if (checkIfInvalid(user.getLastName())) {
+                throw new userException("Last name must be between 1-128 characters.");
+            }
+        } catch (NullPointerException e) {
+            throw new userException("User formatted incorrectly please provide the following:\n" +
+                    "username, email, first_name, last_name");
+        }
+    }
+
+    public void checkPostInputs(User user) throws userException {
+        if (user.getUserID() != null) {
+            throw new userException("Do not provide user_id.");
+        }
+        checkInputs(user);
+    }
+
+    public void checkPutInputs(User user) throws userException {
+        if (user.getUserID() == null) {
+            throw new userException("User formatted incorrectly please provide the following:\n" +
+                    "user_id, username, email, first_name, last_name");
+        }
+        checkInputs(user);
     }
 }
