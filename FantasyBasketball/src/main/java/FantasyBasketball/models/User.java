b72cdd45b4a6c8f84983d7a339cabab71dc544d6
Daktor99 <ed2918@columbia.edu>
2021-11-13 12:37:26 -0500

    Code formatting

--- FantasyBasketball/src/main/java/FantasyBasketball/models/User.java
+++ FantasyBasketball/src/main/java/FantasyBasketball/models/User.java
@@ -13,7 +13,6 @@ public class User {
     // Data members
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty("user_id")
     @Column(name = "user_id")
     private Integer userID;
