eb1c5848ad9d2900e6e825bcee0cfe0b7970023e
tanaymurdia <35699823+tanaymurdia@users.noreply.github.com>
2021-12-08 00:57:19 -0500

    Added steal_weight in client and its functionality in FantasyStatsUtility

--- FantasyBasketball/src/main/java/FantasyBasketball/models/Client.java
+++ FantasyBasketball/src/main/java/FantasyBasketball/models/Client.java
@@ -73,7 +73,7 @@ public class Client {
 
     @Column(name = "steal_weight")
     @JsonProperty("steal_weight")
-    private Integer steal_weight;
+    private double steal_weight;
 
     public Client() {
         this.clientID = null;
@@ -90,6 +90,7 @@ public class Client {
         this.min_league_dur = 1;
         this.max_team_size = 30;
         this.min_league_size = 2;
+        this.steal_weight=1.0;
         this.google_id = null;
     }
 
@@ -97,17 +98,18 @@ public class Client {
                   String email,
                   String company_name,
                   String client_name,
-                  Integer three_p_weight,
-                  Integer two_p_weight,
-                  Integer ft_weight,
-                  Integer rebound_weight,
-                  Integer assist_weight,
-                  Integer block_weight,
+                  double three_p_weight,
+                  double two_p_weight,
+                  double ft_weight,
+                  double rebound_weight,
+                  double assist_weight,
+                  double block_weight,
+                  double steal_weight,
                   Integer turnover_weight,
                   Integer min_league_dur,
                   Integer max_team_size,
                   Integer min_league_size,
-                  String google_id) {
+                  String google_id ) {
         this.clientID = clientID;
         this.email = email;
         this.company_name = company_name;
@@ -119,6 +121,7 @@ public class Client {
         this.assist_weight = assist_weight;
         this.block_weight = block_weight;
         this.turnover_weight = turnover_weight;
+        this.steal_weight=steal_weight;
         this.min_league_dur = min_league_dur;
         this.max_team_size = max_team_size;
         this.min_league_size = min_league_size;
@@ -212,6 +215,14 @@ public class Client {
 
     public void setTurnover_weight(double turnover_weight) {
         this.turnover_weight = turnover_weight;
+    }
+
+    public double getSteal_weight() {
+        return steal_weight;
+    }
+
+    public void setSteal_weight(double steal_weight) {
+        this.steal_weight = steal_weight;
     }
 
     public Integer getMin_league_dur() {
