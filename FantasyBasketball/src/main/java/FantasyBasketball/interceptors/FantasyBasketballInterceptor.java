fd3d0185c8255345c4263d9677b92e538cb4a14d
Daktor99 <ed2918@columbia.edu>
2021-12-10 02:21:16 -0500

    Added interceptor to process client information before request is processed

---
+++ FantasyBasketball/src/main/java/FantasyBasketball/interceptors/FantasyBasketballInterceptor.java
@@ -0,0 +1,48 @@
+package FantasyBasketball.interceptors;
+
+import FantasyBasketball.models.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import FantasyBasketball.services.clientService;
+
+import java.util.List;
+
+@Component
+public class FantasyBasketballInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    clientService clientService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        String token = request.getHeader("token");
+        boolean registerPage = request.getRequestURI().equals("/register");
+        if (token == null && !registerPage) {
+            response.setStatus(401);
+            response.getWriter().write("Token not found, please provide token to use Fantasy Basketball application. "
+                    + "\n\tToken should be provided as 'token' under headers."
+                    + "\n\tIf you don't have an account, you can register on the '/register' route.");
+            return false;
+        }
+
+        List<Client> clientCheck = clientService.getClientByGoogleId(token);
+        if (clientCheck.size() != 1) {
+            response.setStatus(401);
+            response.getWriter().write("Invalid token provided. Please try again.");
+            return false;
+        }
+
+        HttpSession session = request.getSession();
+        session.setAttribute("client", clientCheck.get(0));
+
+        return true;
+    }
+}
